0271dc2ff3496ad5f3ff5730353c6157ba036b8e
Jim He <hemaka@gmail.com>
2017-10-31 10:59:06 -0700

    no message

---
+++ reef.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+
+class Reef():
+	def __init__(self):
+		self.temp = 0.0
+		self.fansStatus = 'off'
+		self.lightStatus = 'off'
+		self.heaterStatus = 'off'
+		
+		base_dir = '/sys/bus/w1/devices/'
+		device_folder = glob.glob(base_dir + '28*')[0]
+		self.device_file = device_folder + '/w1_slave'
+
+	def read_temp_raw():
+	    f = open(self.device_file, 'r')
+	    lines = f.readlines()
+	    f.close()
+	    return lines
+
+	def readTemp():
+    	lines = self.read_temp_raw()
+	    while lines[0].strip()[-3:] != 'YES':
+	        time.sleep(0.2)
+	        lines = self.read_temp_raw()
+	    equals_pos = lines[1].find('t=')
+	    if equals_pos != -1:
+	        temp_string = lines[1][equals_pos+2:]
+	        temp_c = float(temp_string) / 1000.0
+	        return temp_c
+	    return 0
---
+++ start.py
@@ -0,0 +1,29 @@
+import sys
+import time
+import RPi.GPIO as GPIO
+import json
+from Reef import reef
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+reef = Reef()
+loopCount = 0
+while True:
+	temp = reef.readTemp()
+	print(temp)
+	# if(temp < 25.5)
+	# 	if(heaterStatus == 'off')
+	# 		switchHeater('on')
+	# 	if(fansStatus == 'on')
+	# 		switchFans('off')
+	# if(temp > 25.8)
+	# 	if(heaterStatus == 'on')
+	# 		switchHeater('off')
+	# 	if(fansStatus == 'off')
+	# 		switchFans('on')
+	# getAction()
+ #    loopCount += 1
+ #    if(loopCount >= 60)
+ #    	postLog()
+    time.sleep(1)
